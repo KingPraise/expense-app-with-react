35d5dddbc32f955c18ff6b7528dcef8ab921db2e
King Praise <kingpraise15@gmail.com>
2025-05-07 04:20:41 +0100

    expense app files

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ExpenseItem.css";
+const ExpenseItem = () => {
+  return (
+    <div className="">
+      <div>
+        <div>March 28th 2021</div>
+      </div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
